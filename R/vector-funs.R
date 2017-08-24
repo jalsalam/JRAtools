292447912e1b3e90dd3827d0f630cadabc1b0986
Jameel Alsalam <jalsalam@gmail.com>
2017-08-24 09:44:53 -0400

    Documentation tweaks to fix warnings

--- R/vector-funs.R
+++ R/vector-funs.R
@@ -1,4 +1,6 @@
-#' Replace an empty vector with something else. Based on `\%||\%` from rlang.
+#' Replace an empty vector with something else.
+#'
+#' Based on `\%||\%` from rlang.
 #'
 #' @param x,y if `x` is empty, will return `y`; otherwise returns `x`.
 #' @export
@@ -6,6 +8,13 @@
 #' @examples
 #' c(1, 2)[1] %|||% NA_real_
 #' c(1, 2)[0] %|||% NA_real_
+#'
+#' df <- data.frame(
+#'   name   = c("a",   "a", "b", "c", "c", "a", "a"),
+#'   var    = c("hgt", "wt", "hgt", "wt", "hgt", "eye", "wt"),
+#'   value  = c(72, 225, 64, 60, 142, 2, 210)
+#' )
+#' df$value[df$name == "b" & df$var == "wt"] %|||% NA_real_
 #'
 `%|||%` <- function (x, y)
 {
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # JRAtools
-Package of my utility functions and vignettes demonstrating 
+Package of my utility functions and some of my common programming idioms (mostly to remind myself!)
+
+## Installation
+You can install the developmnet version from github with:
+
+```r
+# install.packages("devtools")
+devtools::install_github("jalsalam/JRAtools")
+```
